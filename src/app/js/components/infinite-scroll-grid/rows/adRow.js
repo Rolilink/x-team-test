dfeafb278b0a282c6a8b177e8b54d8b154c87249
Rolando Perez <me@rolilink.com>
2017-04-09 20:35:23 -0500

    reusing Ad inside AdRow and adding tests

--- src/app/js/components/infinite-scroll-grid/rows/adRow.js
+++ src/app/js/components/infinite-scroll-grid/rows/adRow.js
@@ -1,5 +1,5 @@
 import React, { PureComponent, PropTypes } from 'react';
-
+import Ad from '../../ad';
 
 export default class AdRow extends PureComponent {
   static propTypes = {
@@ -10,14 +10,10 @@ export default class AdRow extends PureComponent {
     return `face-row-${this.props.id}`;
   }
 
-  get adSrc() {
-    return `/ad/?r=${this.props.id}`;
-  }
-
   render() {
     return (
       <tr className="ad-row" key={this.key}>
-        <td colSpan="4"><img alt="ad" className="ad center-block img-thumbnail" src={this.adSrc} /></td>
+        <td colSpan="4"><Ad id={this.props.id} /></td>
       </tr>
     );
   }
--- test/components/infinite-scroll-grid/rows/adRow.js
+++ test/components/infinite-scroll-grid/rows/adRow.js
@@ -2,6 +2,7 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import AdRow from '../../../../src/app/js/components/infinite-scroll-grid/rows/adRow';
+import Ad from '../../../../src/app/js/components/ad';
 
 const props = {
   id: 232,
@@ -16,5 +17,9 @@ describe('<AdRow/>', () => {
 
   it('should have a class ad-row', () => {
     expect(wrapper).to.have.className('ad-row');
+  });
+
+  it('should render <Ad /> with prop.id', () => {
+    expect(wrapper).to.have.contain(<Ad id={props.id} />);
   });
 });
